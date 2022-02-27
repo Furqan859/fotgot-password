1685cec1151ff1fb7cfc5625b558c11f0ec60e2b
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-02-27 22:27:06 +0500

    forgot page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import {Box, Button, Container, CssBaseline, Grid, TextField, ThemeProvider, Typography} from '@mui/material'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ return(
 
-export default App;
+
+
+
+<React.Fragment>
+    <CssBaseline />
+<Container maxWidth={'sm'} >
+
+<Box display="flex"  direction="column"
+alignItems="center"
+justifyContent="center"
+style={{ minHeight: '100vh' }}>
+<Container >
+
+  <Grid container spacing={2}   justifyContent='center' alignSelf='center' sx={{display: 'flex',flexDirection:'column' ,mt:'8px',mb:'16px' }}>
+  
+  
+  
+   <Grid item  maxWidth={'sm'} justifyContent='center' align> <Typography fontWeight='550' gutterBottom variant="h5" component="div">
+  Forgot Your Password?
+</Typography></Grid>
+
+  <Grid item><Typography fontWeight='500' sx={{ fontSize: 14  }} gutterBottom color="text.secondary" gutterBottom>
+  Please Enter the email address associated with your your account and we will eamil you a link to reset your password
+  </Typography></Grid>
+  <Grid item><TextField fullWidth label="Email address" id="fullWidth" placeholder="demo@minimals.cc"/></Grid>
+ 
+
+
+  
+
+   <Grid item>
+   <Button  fullWidth sx={{backgroundColor:'black',height:'50px',color:'white' }}>Reset Password</Button>
+   </Grid>
+
+   <Grid item>
+   <Button  fullWidth sx={{backgroundColor:'black',color:'white',height:'50px'}}>Back</Button>
+   </Grid>
+   
+   
+   
+  </Grid>
+  </Container>
+  
+  </Box>
+ 
+  
+  </Container>
+
+  </React.Fragment>
+
+ 
+ 
+ )
+  }
+  export default App;
